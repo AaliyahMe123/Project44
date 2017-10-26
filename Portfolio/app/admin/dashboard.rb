75ff7dc4b6856e4f9f2082b0e425920525bd3a25
Aaliyah Melton <aaliyahmariah09@gmail.com>
2017-10-26 10:22:16 -0400

    Editing Dashboard

--- Portfolio/app/admin/dashboard.rb
+++ Portfolio/app/admin/dashboard.rb
@@ -1,17 +1,17 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  # menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+  # content title: proc{ I18n.t("active_admin.dashboard") } do
+  #   div class: "blank_slate_container", id: "dashboard_default_message" do
+  #     span class: "blank_slate" do
+  #       span I18n.t("active_admin.dashboard_welcome.welcome")
+  #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #     end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
-    #
+    
     # columns do
     #   column do
     #     panel "Recent Posts" do
@@ -25,9 +25,9 @@ ActiveAdmin.register_page "Dashboard" do
 
     #   column do
     #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
+    #       para "Welcome"
     #     end
     #   end
     # end
-  end # content
-end
+  # end # content
+# end
